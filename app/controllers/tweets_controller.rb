c783db981d8fdd9f28e328712176c5fa3dc4c169
abdi-hu <78183395+abdi-hu@users.noreply.github.com>
2021-04-23 12:08:54 -0400

    Adds tweets route and created controller with index and show

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,13 @@
+class TweetsController < ApplicationController
+
+    def index
+        tweets = Tweet.all
+        render(json: {tweets: tweets})
+    end
+
+    def show
+        tweet = Tweet.find(params[:id])
+        render(json: {tweet: tweet})
+    end
+
+end
