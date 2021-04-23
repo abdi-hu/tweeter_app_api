2f9e6c3ea789ba4398005a7a6dffa50a9d41b095
abdi-hu <78183395+abdi-hu@users.noreply.github.com>
2021-04-23 12:18:38 -0400

    Can POST new Tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,4 +10,21 @@ class TweetsController < ApplicationController
         render(json: {tweet: tweet})
     end
 
+    def create
+        tweet = Tweet.new(tweet_params)
+
+        if tweet.save
+            render json: {tweet: tweet}
+        else
+            render(status: 422, json: {tweet: tweet, errors: song.errors})
+        end
+        
+    end
+
+    private
+
+    def tweet_params
+        params.require(:tweet).permit(:title, :content, :author)
+    end
+
 end
